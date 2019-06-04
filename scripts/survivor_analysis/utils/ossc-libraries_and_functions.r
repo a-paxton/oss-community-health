457b25c0ad094c952866e2148e6c3319e2590d68
Alexandra Paxton <paxton.alexandra@gmail.com>
2019-06-04 19:48:40 -0400

    Fix pander_lme file call

--- scripts/survivor_analysis/utils/ossc-libraries_and_functions.r
+++ scripts/survivor_analysis/utils/ossc-libraries_and_functions.r
@@ -38,6 +38,6 @@ invisible(lapply(required_packages, library, character.only = TRUE))
 options(scipen=999)
 
 # read in pander_lme
-pander_lme_url = "https://raw.githubusercontent.com/NelleV/a-paxton/0b25835e65bc2e6be552b755d8aa69fc541bab23/pander_lme.R"
+pander_lme_url = "https://raw.githubusercontent.com/a-paxton/stats-tools/2a1bf715097bbcc966ab612af3a9e0b14408d4ff/pander_lme.R"
 pander_lme_file = getURL(pander_lme_url, ssl.verifypeer = FALSE)
 eval(parse(text = pander_lme_file))
