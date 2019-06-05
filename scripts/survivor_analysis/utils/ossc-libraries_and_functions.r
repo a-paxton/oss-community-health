ab7a13f485c6a55b7b9a9710d6fd16952607a90c
Alexandra Paxton <paxton.alexandra@gmail.com>
2019-06-05 11:28:32 -0400

    Move all functions created in the Rmd to the  R script

--- scripts/survivor_analysis/utils/ossc-libraries_and_functions.r
+++ scripts/survivor_analysis/utils/ossc-libraries_and_functions.r
@@ -41,3 +41,61 @@ options(scipen=999)
 pander_lme_url = "https://raw.githubusercontent.com/a-paxton/stats-tools/2a1bf715097bbcc966ab612af3a9e0b14408d4ff/pander_lme.R"
 pander_lme_file = getURL(pander_lme_url, ssl.verifypeer = FALSE)
 eval(parse(text = pander_lme_file))
+
+compute_p_value_from_t_stats = function(t_stats){
+  # From Alex -- this is the norm in Psychology, and should be more
+  # conservative than trying to estimate the number of degrees of freedoms
+  # using Welch-Sattherwaite equation.
+  p_values = 2*(1-pnorm(abs(t_stats)))
+}
+
+pander_clean_anova = function(model, rename_columns=TRUE,
+                              pval_correction_method="BH"){
+  # First, rename the functions into something sane.
+  if(rename_columns){
+    colnames(model) = c("sum_sq", "mean_sq", "num_df", "den_df",
+                        "f_value", "p_value")
+  }
+  
+  # Adjust p-values
+  model$p_adj = stats::p.adjust(model$p_value, method=pval_correction_method)
+  
+  # Now, apply the same rounding on the p_value than on the rest
+  model$p_value[model$p_value < .0001] = .0001
+  model$p_value[model$p_value >= .0001] = round(model$p_value[model$p_value >= .0001],4)
+  model$p_value[model$p_value >= .0005] = round(model$p_value[model$p_value >= .0005],3)
+  model$p_value[model$p_value >= .25] = round(model$p_value[model$p_value >= .25],2)
+  
+  model$p_adj[model$p_adj < .0001] = .0001
+  model$p_adj[model$p_adj >= .0001] = round(model$p_adj[model$p_adj >= .0001],4)
+  model$p_adj[model$p_adj >= .0005] = round(model$p_adj[model$p_adj >= .0005],3)
+  model$p_adj[model$p_adj >= .25] = round(model$p_adj[model$p_adj >= .25],2)
+  
+  model$sig = ' '
+  model$sig[model$p_adj < .1] = '.'
+  model$sig[model$p_adj < .05] = '*'
+  model$sig[model$p_adj < .01] = '**'
+  model$sig[model$p_adj < .001] = '***'
+  return(pander(model, , split.table = Inf, style = 'rmarkdown'))
+}
+
+#' Welch t-test
+compute_t_statistics = function(means, standard_error,
+                                contrasts){
+  all_tests = data.frame()
+  for(contrast in contrasts){
+    splits = unlist(strsplit(contrast, "-"))
+    if(length(splits) != 2){
+      error("Dunno how to deal with this just yet") 
+    }
+    
+    group1 = splits[1]
+    group2 = splits[2]
+    
+    t = ((means[group1] - means[group2]) /
+           (standard_error[group1]**2 +
+              standard_error[group2]**2)**.5)
+    all_tests[contrast, "t_stats"] = t
+  }
+  return(all_tests) 
+}
